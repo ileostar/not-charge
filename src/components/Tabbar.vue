2a8863f16e721092575ff908c3198c0a7545aed9
ileostar <hi@leostar.top>
2024-05-05 22:39:57 +0800

    chore: remove eslint warning

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
-
-const getActionClass = (param: string)=>{
+function getActionClass(param: string) {
   const routes = getCurrentPages()
-  console.log(routes[routes.length - 1].route?.split('/')[1])
-  return routes[routes.length - 1].route?.split('/')[1]===param ?'color-green text-7 mb-5':'mb-5'
+  return routes[routes.length - 1].route?.split('/')[1] === param ? 'color-green text-7 mb-5' : 'mb-5'
 }
 </script>
 
 <template>
-  <view border-box h-23 w-full flex text="6.5" justify-around items-center bg="white/10" rd-5>
+  <view text="6.5" bg="white/10" border-box h-23 w-full flex items-center justify-around rd-5>
     <navigator :class="getActionClass('index')" url="/pages/index" mb-5>
-      <span i-carbon:home/>
+      <span i-carbon:home />
     </navigator>
     <navigator :class="getActionClass('report')" url="/pages/report" mb-5>
-      <span i-carbon:ibm-secure-infrastructure-on-vpc-for-regulated-industries/>
+      <span i-carbon:ibm-secure-infrastructure-on-vpc-for-regulated-industries />
     </navigator>
     <navigator :class="getActionClass('user')" url="/pages/user" mb-5>
-      <span i-carbon:user/>
+      <span i-carbon:user />
     </navigator>
   </view>
 </template>
