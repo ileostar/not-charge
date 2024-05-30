bd17f9ed09ba33974cfd58abd41a30df2f1933a4
Feng <1661787635@qq.com>
2024-05-30 22:18:23 +0800

    chore: update

---
+++ src/pages/myQRCode.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view>myQRCode</view>
+</template>
+
+<style>
+
+</style>
---
+++ src/pages/scanQRCode.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view>scanQRCode</view>
+</template>
+
+<style>
+
+</style>
