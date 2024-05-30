91392785dee38b69230b50546f08eb0145e58357
Feng <1661787635@qq.com>
2024-05-30 19:03:27 +0800

    the userStaticPage have done

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -17,7 +17,7 @@
           </text>
         </view>
       </view>
-      <button class="sign-in-button w-15 rounded-r-full bg-yellow-500 px-4 py-2 text-white">
+      <button class="sign-in-button h-6 w-9 rounded-full bg-yellow-500 px-4 py-2 text-white">
         签到
       </button>
     </view>
@@ -52,36 +52,90 @@
 
     <!-- 中间部分 -->
     <view class="middle-section mt-2 bg-white">
-      <navigator url="/pages/accountManagement" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>账本管理</text>
+      <navigator url="/pages/accountManagement" class="block flex items-center justify-between border-b border-gray-200 border-b-style-solid px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:notebook-and-pen />
+          <text class="ml-1.25">
+            账本管理
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
-      <navigator url="/pages/importAccount" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>账本导入</text>
+      <navigator url="/pages/importAccount" class="block flex items-center justify-between border-b border-gray-200 border-b-style-solid px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:file-addition />
+          <text class="ml-1.25">
+            账本导入
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
-      <navigator url="/pages/budget" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>我的预算</text>
+      <navigator url="/pages/budget" class="block flex items-center justify-between border-b border-gray-200 border-b-style-solid px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:financing-one />
+          <text class="ml-1.25">
+            我的预算
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
-      <navigator url="/pages/bankCards" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>银行卡</text>
+      <navigator url="/pages/bankCards" class="block flex items-center justify-between border-b border-gray-200 border-b-style-solid px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:bank-card />
+          <text class="ml-1.25">
+            银行卡
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
-      <navigator url="/pages/reminders" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>定时提醒</text>
+      <navigator url="/pages/reminders" class="block flex items-center justify-between border-b border-gray-200 border-b-style-solid px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:alarm-clock />
+          <text class="ml-1.25">
+            定时提醒
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
-      <navigator url="/pages/salaryCalculator" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>工资计算器</text>
+      <navigator url="/pages/salaryCalculator" class="block flex items-center justify-between border-b border-gray-200 px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:calculator-one />
+          <text class="ml-1.25">
+            工资计算器
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
     </view>
 
     <!-- 下部分 -->
     <view class="bottom-section mt-2 bg-white">
-      <navigator url="/pages/gestureLock" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>手势密码</text>
+      <navigator url="/pages/gestureLock" class="block flex items-center justify-between border-b border-gray-200 border-b-style-solid px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:electronic-locks-close />
+          <text class="ml-1.25">
+            手势密码
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
-      <navigator url="/pages/about" class="block flex items-center border-b border-gray-200 px-4 py-3">
-        <text>关于</text>
+      <navigator url="/pages/about" class="block flex items-center justify-between border-b border-gray-200 border-b-style-solid px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:trumpet />
+          <text class="ml-1.25">
+            关于
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
-      <navigator url="/pages/settings" class="block flex items-center px-4 py-3">
-        <text>设置</text>
+      <navigator url="/pages/settings" class="block flex items-center justify-between px-4 py-3">
+        <view class="flex items-center">
+          <span class="icon iconify" i-icon-park:setting />
+          <text class="ml-1.25">
+            设置
+          </text>
+        </view>
+        <span i-icon-park:go-end />
       </navigator>
     </view>
   </view>
@@ -96,7 +150,14 @@
   height: 55px;
 }
 .sign-in-button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
   min-width: 70px;
   border-radius: 9999px; /* 半圆按钮 */
+  font-size: 15px
+}
+.iconify {
+  color: #f8c43d; /* 默认颜色，可以根据需要修改 */
 }
 </style>
